f00039ea9ca6f3982048bd3a894da5eb6627d827
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-05-26 16:43:26 +0500

    Fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
-const menuBtn = document.querySelector('#menuBtn');
-const sidebarMenu = document.querySelector('#menu');
+const menuBtn = document.getElementById('menuBtn');
+const sidebarMenu = document.getElementById('sidebar');
 
 menuBtn.addEventListener('click', () => {
-  sidebarMenu.classList.toggle('hidden');
+  sidebarMenu.classList.toggle('-translate-x-full');
 });
