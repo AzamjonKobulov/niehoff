b474d864bbcc8310a30ad28da179b69d187f2284
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-05-26 16:52:22 +0500

    Fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.getElementById('menuBtn');
 const sidebarMenu = document.getElementById('sidebar');
+const body = document.getElementsByTagName('body');
 
 menuBtn.addEventListener('click', () => {
   sidebarMenu.classList.toggle('hidden');
